9766d2ea0dc6fe43cb3d824278f805973093f9da
Delta de Dirac <email@example.com>
2021-03-02 23:23:49 +0200

    contact - side menu + fix change pic

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function addPlaceholder() {
   document.getElementById("search").placeholder = "רשום מילת חיפוש";
 }
 
-setInterval(function () {
-  document.getElementById("ads-img").src = "img/ads-" + ((counter++ % 4) + 1) + ".jpg";
-}, 10000);
+function changePic() {
+  setInterval(function () {
+    document.getElementById("ads-img").src = "img/ads-" + ((counter++ % 4) + 1) + ".jpg";
+  }, 10000);
+}
