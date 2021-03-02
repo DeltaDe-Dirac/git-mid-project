7fc4084b7b84c231a789b51c0cf4d78df4ecc4ed
Delta de Dirac <email@example.com>
2021-03-02 21:08:43 +0200

    add js - change ads and search placeholder

---
+++ script.js
@@ -0,0 +1,13 @@
+let counter = 1;
+
+function removePlaceholder() {
+  document.getElementById("search").placeholder = "";
+}
+
+function addPlaceholder() {
+  document.getElementById("search").placeholder = "רשום מילת חיפוש";
+}
+
+setInterval(function () {
+  document.getElementById("ads-img").src = "img/ads-" + ((counter++ % 4) + 1) + ".jpg";
+}, 10000);
